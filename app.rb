4fa191cc23ca1e9cc78742ae5ccd7773f408b0df
kazuma.yonetani <kazuma.yonetani@classi.jp>
2020-03-19 12:50:58 +0900

    ルーティングを追加

--- app.rb
+++ app.rb
@@ -1,6 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+app_name = "Memo App"
+
+# Top
 get '/' do
-  'memo app'
+  app_name
 end
+
+# New
+get '/new' do
+  "New Memo - #{app_name}"
+end
+
+# Edit
+get "/*/edit" do |id|
+  "Edit #{id} - #{app_name}"
+end
+
+# Show
+get '/*' do |id|
+  "Show #{id} - #{app_name}"
+end
+
