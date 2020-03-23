8d84abdc881beed2d59a25769ce2d2ebaee43c02
kazuma.yonetani <kazuma.yonetani@classi.jp>
2020-03-23 12:48:24 +0900

    Revert "パス変換をメソッドにした"
    This reverts commit eb8a9dffa3f05e4d69228940d05244d4261ad6f9.

--- app.rb
+++ app.rb
@@ -20,23 +20,20 @@ class Memo
       index
     end
     def create(text)
-      File.open(to_path(latest_id + 1), "w") {|f| f.puts(text)}
+      File.open("data/#{latest_id + 1}.txt", "w") {|f| f.puts(text)}
     end
     def read(id)
-      File.open(to_path(id), "r") {|f| f.read}
+      File.open("data/#{id}.txt", "r") {|f| f.read}
     end
     def update(id, text)
-      File.open(to_path(id), "w") {|f| f.puts(text)}
+      File.open("data/#{id}.txt", "w") {|f| f.puts(text)}
     end
     def delete(id)
-      File.delete(to_path(id))
+      File.delete("data/#{id}.txt")
     end
     private
     def latest_id
       Dir.glob("[0-9]*.txt", base: "data").map{|f| f.delete(".txt").to_i}.max || 0
-    end
-    def to_path(id)
-      "data/#{id}.txt"
     end
   end
 end
