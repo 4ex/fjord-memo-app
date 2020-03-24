c76b56e3bb2b987e3dcaa79c50161410640866a2
kazuma.yonetani <kazuma.yonetani@classi.jp>
2020-03-24 16:46:22 +0900

    rubocopに注意された箇所を修正

--- lib/memo.rb
+++ lib/memo.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class Memo
   attr_reader :id
-  
+
   def initialize(id)
     @id = id
   end
@@ -14,19 +16,19 @@ class Memo
   class << self
     def index
       index = {}
-      data_files.each do |f|
-        index[to_id(f)] = File.open(f){|f| f.gets}
+      data_files.each do |file|
+        index[to_id(file)] = File.open(file) { |f| f.gets }
       end
       index
     end
     def create(text)
-      File.open(to_path(latest_id + 1), "w") {|f| f.puts(text)}
+      File.open(to_path(latest_id + 1), "w") { |f| f.puts(text) }
     end
     def read(id)
-      File.open(to_path(id), "r") {|f| f.read}
+      File.open(to_path(id), "r") { |f| f.read }
     end
     def update(id, text)
-      File.open(to_path(id), "w") {|f| f.puts(text)}
+      File.open(to_path(id), "w") { |f| f.puts(text) }
     end
     def delete(id)
       File.delete(to_path(id))
@@ -37,10 +39,10 @@ class Memo
 
     private
     def data_files
-      Dir.glob("data/[0-9]*.txt").sort_by {|f| File.mtime(f)}.reverse
+      Dir.glob("data/[0-9]*.txt").sort_by { |f| File.mtime(f) }.reverse
     end
     def latest_id
-      data_files.map{|f| to_id(f)}.max || 0
+      data_files.map { |f| to_id(f) }.max || 0
     end
     def to_path(id)
       "data/#{id}.txt"
